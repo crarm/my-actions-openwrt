96c995afad83543ef6abb8b127bfd516a4f0bc25
crarm <61692711+crarm@users.noreply.github.com>
2020-03-04 11:32:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 #=======================================
-#删除lienol与lean包中的重复程序
+#delete the same packages bettwen lienol and lean 
 #=======================================
-#rm -rf package/lienol/lienol/ipt2socks
-#rm -rf package/lienol/lienol/shadowsocksr-libev
-#rm -rf package/lienol/lienol/pdnsd-alt
-#rm -rf package/lienol/package/verysync
-#rm -rf package/lienol/package/ipt2socks
-#rm -rf package/lienol/lienol/luci-app-verysync
-#rm -rf package/lienol/package/kcptunkcptun
+rm -rf package/lienol/lienol/ipt2socks
+rm -rf package/lienol/lienol/shadowsocksr-libev
+rm -rf package/lienol/lienol/pdnsd-alt
+rm -rf package/lienol/package/verysync
+rm -rf package/lienol/package/ipt2socks
+rm -rf package/lienol/lienol/luci-app-verysync
+rm -rf package/lienol/package/kcptunkcptun
 rm -rf package/lean/pdnsd-alt
 rm -rf package/lean/shadowsocksr-libev
 rm -rf package/lean/simple-obfs
@@ -23,29 +22,17 @@ rm -rf package/lean/luci-app-pptp-server
 rm -rf package/lean/luci-app-ramfree
 rm -rf package/lean/luci-app-softethervpn
 rm -rf package/lean/luci-app-v2ray-server
-
-#修改默认内网网关IP地址
+#config the IP 
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
-
-#luci-app-ssr-plus 改为 vpn 菜单下
+#change luci-app-ssr-plus to vpn menu
 sed -i 's/"services"/"vpn"/g' package/lean/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
-
-
-#luci-app-openclash 改为 vpn 菜单下
+#change luci-app-openclash to vpn menu
 sed -i 's/"services"/"vpn"/g' package/OpenClash/luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
-
-
-#luci-app-frpc 改为vpn 菜单下
+#change luci-app-frpc to vpn menu
 sed -i 's/"services"/"vpn"/g' package/lean/luci-app-frpc/luasrc/controller/frp.lua
-
-
-#luci-app-nps 改为vpn 菜单下
+#change luci-app-nps to vpn menu
 sed -i 's/"services"/"vpn"/g' package/lean/luci-app-nps/luasrc/controller/nps.lua
-
-
-#luci-app-openvpn-server 改为vpn 菜单下
+#change luci-app-openvpn-server to vpn menu
 sed -i 's/"services"/"vpn"/g' package/lean/luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
-
-#luci-app-openvpn 改为vpn 菜单下
+#change luci-app-openvpn to vpn menu
 #sed -i 's/"services"/"vpn"/g' package/lean/luci-app-openvpn/luasrc/controller/openvpn.lua
-
